980115f5e65daa3d8c44e0b445c3ac8f737f7f70
timothy adigun <adigun.timothy@gmail.com>
2026-06-07 23:26:27 +0100

    using selectors

--- learning_jquery/ch01/selectors.js
+++ learning_jquery/ch01/selectors.js
@@ -1,6 +1,6 @@
 "use strict";
 
 $(() => {
-  $("#selected-plays > li").addClass("horizontal");
+  $("#selected-plays > li").addClass("horizontal").addClass("big-letter");
   $("#selected-plays li:not(.horizontal)").addClass("sub-level");
 });
